d4ee20f939cf9b52149b77404a5ba13cb9c9066a
HerrWal <85037202+HerrWal@users.noreply.github.com>
2022-03-31 21:09:07 -0400

    Update Phrase.js

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,32 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor (phrase) {
+        this.phrase = phrase.toLowerCase();        
+    }
+    /**
+     * Display phrase on game board
+     */
+    addPhraseToDisplay() {
+        const phraseSectionUl = document.getElementById('phrase').firstElementChild;        
+        for (let i = 0; i < this.phrase.length; i++ ) {
+            let li;            
+            if (this.phrase[i] === ' ') {
+                li = `<li class="space"> </li>`;
+            } else {
+                li = `<li class="hide letter ${this.phrase[i]}">${this.phrase[i]}</li>`;
+            }
+            phraseSectionUl.insertAdjacentHTML('beforeend', li);                       
+        }                
+    }
+    /**
+     * checkLetter() method
+     */
+    checkedLetter() {}
+    /**
+     * showMatchedLetter()
+     */
+    showMatchedLetter() {}
+}
