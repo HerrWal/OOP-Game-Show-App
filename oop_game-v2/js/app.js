626588d76c033a97b6b733e7a4c240b0cd21cb01
HerrWal <85037202+HerrWal@users.noreply.github.com>
2022-03-31 21:08:58 -0400

    Update app.js

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,3 +1,15 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+// const logPhrase = (phrase) => {
+//     console.log(`Phrase - phrase: `, phrase.phrase);
+//     };
+    const game = new Game();
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+
+    
+game.getRandomPhrase().addPhraseToDisplay();
