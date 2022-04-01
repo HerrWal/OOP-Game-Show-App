0476474a7c627d551ecfe8b49955f92617f23e92
HerrWal <85037202+HerrWal@users.noreply.github.com>
2022-03-31 21:09:04 -0400

    Update Game.js

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      new Phrase("Hello World"),
+      new Phrase("To the infinity and the end"),
+      new Phrase("El mejor pais de Chile"),
+      new Phrase("Praise the Sun"),
+      new Phrase("Vae victis"),
+    ];
+    this.activatePhrase = null;
+  }
+  /**
+   * Selects random phrase from phrases property
+   * @return {object} Phrase pbject chosen to be used
+   */
+  getRandomPhrase() {
+      const randomPhrase = this.phrases[Math.floor(Math.random()*this.phrases.length)];
+      return randomPhrase;
+  };
+}
