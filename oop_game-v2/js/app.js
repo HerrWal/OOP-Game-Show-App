f5c5fd96ebc64d8e616cf92ec1b69b3890dc9992
HerrWal <85037202+HerrWal@users.noreply.github.com>
2022-04-07 20:20:42 -0400

    Update app.js

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -8,18 +8,31 @@
 let game;
 
 document.getElementById("btn__reset").addEventListener("click", () => {
+  document.querySelector("#phrase > ul").innerHTML = "";
+  const keys = document.querySelectorAll(".key");
+  for (let key of keys) {
+    key.removeAttribute("disabled");
+    key.classList.remove("chosen", "wrong");
+  }
+  const lives = document.querySelectorAll(".tries > img");
+  for (let live of lives) {
+    live.src = "images/liveHeart.png";
+  }
   game = new Game();
-  game.startGame();  
+  game.startGame();
 });
-
-/**
- * Now it’s time to start adding some user interaction to the game. When a user clicks on one of
-the onscreen keyboard buttons, several things need to happen:
-● The clicked/chosen letter must be captured.
-● The clicked letter must be checked against the phrase for a match.
-● If there’s a match, the letter must be displayed on screen instead of the placeholder.
-● If there’s no match, the game must remove a life from the scoreboard.
-● The game should check if the player has won the game by revealing all of the letters in
-the phrase or if the game is lost because the player is out of lives.
-● If the game is won or lost, a message should be displayed on screen.
- */
+const qwerty = document.getElementById("qwerty");
+qwerty.addEventListener("click", () => {
+  if (event.target.tagName === "BUTTON") {
+    game.handleInteraction(event.target);
+  }
+});
+document.addEventListener("keydown", () => {
+  const keys = document.querySelectorAll(".key");
+  for (let key of keys) {
+    if(key.textContent === event.key) {
+      game.handleInteraction(key);
+    }
+  }
+ console.log(event.key);
+});
