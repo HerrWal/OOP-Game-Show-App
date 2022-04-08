5323d65717b0f3876bbbde869d5d4480627c88e9
HerrWal <85037202+HerrWal@users.noreply.github.com>
2022-04-07 20:20:53 -0400

    Update Game.js

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -8,10 +8,10 @@ class Game {
     this.phrases = [
       new Phrase("Hello World"),
       new Phrase("To the infinity and the end"),
-      new Phrase("El mejor pais de Chile"),
+      new Phrase("Say Hello to my little friend"),
       new Phrase("Praise the Sun"),
       new Phrase("Vae victis"),
-      new Phrase("Te amo moshita bella")
+      new Phrase("I love programming"),
     ];
     this.activePhrase = null;
   }
@@ -20,27 +20,33 @@ class Game {
    * @return {object} Phrase pbject chosen to be used
    */
   getRandomPhrase() {
-      const randomPhrase = this.phrases[Math.floor(Math.random()*this.phrases.length)];
-      return randomPhrase;
-  };
+    const randomPhrase =
+      this.phrases[Math.floor(Math.random() * this.phrases.length)];
+    return randomPhrase;
+  }
   /**
    * Begins game by selecting a random phrase and displaying it to user
    */
   startGame() {
-      document.getElementById('overlay').style.display = 'none';
-      const phrase = this.getRandomPhrase();
-      this.activePhrase = phrase;
-      return phrase.addPhraseToDisplay();      
-  };
+    document.getElementById("overlay").style.display = "none";
+    const phrase = this.getRandomPhrase();
+    this.activePhrase = phrase;
+    return phrase.addPhraseToDisplay();
+  }
   /**
    * Checks for winning move
    * @return {boolean} True if game has been won, false otherwise
    */
   checkForWin() {
     const allLettersCount = document.querySelectorAll(".letter").length;
-    const ShowedLettersCount = document.getElementsByClassName('show').length; 
-    return (allLettersCount == ShowedLettersCount) ? true : false;
-  };
+    const ShowedLettersCount = document.getElementsByClassName("show").length;
+    if (allLettersCount == ShowedLettersCount) {
+      this.gameOver(true);
+      return true;
+    } else {
+      return false;
+    }
+  }
   /**
    * Increases the value of the missed property
    * Removes a life from the scoreboard
@@ -48,36 +54,64 @@ class Game {
    */
   removeLife() {
     // const tries = document.getElementsByClassName('tries');
-    const tries = document.querySelectorAll('.tries > img');
-    for(let i = 0; i <= this.missed; i++ ) {
-      tries[i].src = 'images/lostHeart.png'
-    }    
-    this.missed == 5 ? this.gameOver() : this.missed ++;   
-  };
+    const tries = document.querySelectorAll(".tries > img");
+    for (let i = 0; i <= this.missed; i++) {
+      tries[i].src = "images/lostHeart.png";
+    }
+    this.missed++;
+    if (this.missed == 5) {
+      this.gameOver(false);
+    }
+  }
   /**
    * Displays game over message
    * @param {boolean} gameWon - whether or not the user won the game
    */
   gameOver(gameWon) {
-    
-  };
+    const overlay = document.getElementById("overlay");
+    overlay.style.display = "";
+    if (gameWon == true) {
+      overlay.classList.remove("start", "lose");
+      overlay.classList.add("win");
+      document.getElementById("game-over-message").textContent = "You Win!";
+    }
+    if (gameWon == false) {
+      overlay.classList.remove("start", "win");
+      overlay.classList.add("lose");
+      document.getElementById("game-over-message").textContent = "You lose!";
+    }
+  }
 
-  handleInteraction() {
-    const qwerty = document.querySelectorAll('qwerty');
-    const keys = document.querySelectorAll(".key");
-    const letters = document.querySelectorAll(".letter");
-    qwerty.addEventListener('click', () => {
-      const selectedKey = event.target;      
-      selectedKey.setAttribute('disabled', '');
-      for (const letter of letters) {
-        if (selectedKey.textContent === letter.textContent) {
-          console.log('is a match!');
-          selectedKey.classList.add('chosen');
-        } else if (selectedKey.textContent !== letter.textContent) {
-          console.log('Popo!');
-          selectedKey.classList.add('wrong');
-        }
-      }     
-    });   
-  };  
+  /**
+   * Handles onscreen keyboard button clicks
+   * @param {HTMLButtonElement} button - The clicked button element
+   */
+  handleInteraction(button) {
+    button.setAttribute("disabled", "");
+    if (this.activePhrase.checkLetter(button.textContent)) {
+      button.classList.add("chosen");
+      this.activePhrase.showMatchedLetter(button.textContent);
+      this.checkForWin();
+    } else {
+      button.classList.add("wrong");
+      this.removeLife();
+    }
+  }
 }
+
+// const qwerty = document.querySelectorAll("qwerty");
+//     const keys = document.querySelectorAll(".key");
+//     const letters = document.querySelectorAll(".letter");
+//     qwerty.addEventListener("click", () => {
+//       const selectedKey = event.target;
+//       selectedKey.setAttribute("disabled", "");
+//       for (const letter of letters) {
+//         if (selectedKey.textContent === letter.textContent) {
+//           console.log("is a match!");
+//           selectedKey.classList.add("chosen");
+//         } else if (selectedKey.textContent !== letter.textContent) {
+//           console.log("Popo!");
+//           selectedKey.classList.add("wrong");
+//         }
+//       }
+//     });
